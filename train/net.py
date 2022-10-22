1d85530d465c3b0c053012637d44ba76b8b896f1
huzhihen <1852647560@qq.com>
2022-10-22 17:20:42 +0800

    first commit

--- train/net.py
+++ train/net.py
@@ -180,17 +180,17 @@ class Decoder1(nn.Module):
         x4_0 = input[4]
 
         x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0), self.down(x0_0), self.down(x0_1)], 1))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0), self.down(x1_0), self.down(x1_1)], 1))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0), self.down(x2_0), self.down(x2_1)], 1))
 
-        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0), self.down(x0_1), self.down(x0_0)], 1))
         x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1), self.down(x0_1), self.down(x0_2)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1), self.down(x1_1), self.down(x1_2)], 1))
 
-        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0), self.down(x1_1), self.down(x1_0)], 1))
-        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1), self.down(x0_2), self.down(x0_1)], 1))
         x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2), self.down(x0_2), self.down(x0_3)], 1))
 
-        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0), self.down(x2_1), self.down(x2_0)], 1))
-        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1), self.down(x1_2), self.down(x1_1)], 1))
-        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2), self.down(x0_3), self.down(x0_3)], 1))
         x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
         return x4_0, x3_1, x2_2, x1_3, x0_4
 
